bbcd55eeafbbf15a5503485431c88c09d0741c34
Bharat bhushan <43534123+bharat6138@users.noreply.github.com>
2023-02-28 15:48:48 +0530

    Update Feed.js

--- src/component/Feed.js
+++ src/component/Feed.js
@@ -1,30 +1,46 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,useLayoutEffect } from "react";
 import { usePosition } from "../utils/usePosition";
 import { fetchDataFromApi } from "../utils/api";
 import Card from "./Card";
 import Loading from "./Loading";
+import Filter from "./Filter";
+import { Outlet , useSearchParams} from "react-router-dom";
 
 const Feed = () => {
 	const { latitude, longitude, error } = usePosition();
 	const [ResturantsList, setResturantsList] = useState([]);
 	const [page, setPage] = useState(16);
 	const [loading, setLoading] = useState(true);
+	const [searchParams, setSearchParams] = useSearchParams();
+    const param = searchParams.get('sortBy');
+	
 	// const { searchQuery } = useParams();
 	useEffect(() => {
 		if (latitude && longitude) fetchSliderList();
-	}, [latitude, longitude, page]);
+	}, [latitude, longitude,param]);
 
-	const fetchSliderList = () => {
+	useEffect(()=>{
+		fetchSliderList(true);
+	},[page])
+	const fetchSliderList = (flag = false) => {
 		// onLoaderFinished(30);
+		// setSearchParams()
 		fetchDataFromApi(
-			`v5?lat=${latitude}&lng=${longitude}&offset=${page}&sortBy=RELEVANCE&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING`,
+			`v5?lat=${latitude}&lng=${longitude}&offset=${page}&sortBy=${param}&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING`,
 		)
 			.then((response) => {
 				// JSON.parse(data);
-				console.log(response?.data?.cards);
+				
+				console.log(response) 	 ;
+				if(response  && response.data){
+					flag  ? setResturantsList( [...ResturantsList  , ...response?.data?.cards]): setResturantsList(response.data.cards)
+					// setResturantsList((prev) => [...prev, ...response?.data?.cards]);
+					setLoading(false);
+				}
 				// onLoaderFinished(60);
-				setResturantsList((prev) => [...prev, ...response?.data?.cards]);
-				setLoading(false);
+				// if(param === response.data.sorts)
+
+				
 				// onLoaderFinished(100);
 			})
 			.catch((error) => console.log(error));
@@ -79,23 +95,29 @@ const Feed = () => {
 	}, []);
 
 	return (
-		<div className=" min-h-[calc(100vh - 368px)] overflow-x-hidden overflow-y-auto relative z-2">
+		<div className=" min-h-[calc(100vh - 368px)] overflow-x-hidden overflow-y-auto relative z-2 fonts-loaded ">
 			<div className=" max-w-[1200px] min-w-[1200px] my-0 mx-auto relative block">
-				<div className="mt-0 mx-auto py-[20px] bg-white relative">
+				<div className="mt-0 mx-auto py-[20px] bg-white">
+				<div className="relative  h-[72px]">
 					<div
-						className=" h-[72px] relative translate-x-[10px] translate-y-[10px] absolute top-0 left-0 right-0 "
+						className="flex items-center translate-x-[10px] translate-y-[10px] absolute top-0 left-0 right-0 "
 						id="myHeader"
 					>
 						<div className=" flex items-center my-0 mx-auto max-w-[1200px] min-w-[1200px] h-[100%]">
-							<h3 className=" font-[600] text-[#282c3f] text-[28px] flex-1 fonts-loaded">
-								restaurants
+							<h3 className=" font-[600] text-[#282c3f] text-[28px] flex-1  h-[100%]">
+								{ResturantsList.length} restaurants {param}
 							</h3>
+							<Filter/>
 						</div>
 					</div>
+					</div>
 					<div className="pt-[40px] mt-[-10px] border-t border-[#e9e9eb]">
-						<div className=" grid grid-cols-4 gap-14 justify-evenly">
+						<div className=" grid grid-cols-4 gap-x-12 justify-evenly">
 							{ResturantsList?.map((restaurant, index) => {
-								return <Card {...restaurant?.data?.data} key={index} />;
+							
+									if (restaurant?.data?.data.aggregatedDiscountInfo !== undefined)  
+								 return <Card {...restaurant?.data?.data} key={index} />
+
 							})}
 							{loading &&
 								Array(4)
