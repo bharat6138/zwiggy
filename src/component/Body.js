904c1a276cc3f28d9e44d1e2e069a9b55f9cbed8
Bharat bhushan <43534123+bharat6138@users.noreply.github.com>
2023-02-28 15:47:50 +0530

    Update Body.js

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,57 +1,21 @@
-import React, { useState, useEffect } from "react";
-import { usePosition } from "../utils/usePosition";
-import { fetchDataFromApi } from "../utils/api";
-import { settings } from "../utils/constant";
-import { IMG_SLIDER_URL } from "../utils/constant";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Feed from "./Feed";
+import ItemSlider from "./ItemSlider";
 
-import Slider from "react-slick";
-import ShimmerSlider from "../shimmer/ShimmerSlider";
-
-const ItemSlider = () => {
-	const { latitude, longitude, error } = usePosition();
-	const [sliderResturantsList, setSliderResturantsList] = useState([]);
-	// const { searchQuery } = useParams();
-	useEffect(() => {
-		if (latitude && longitude) fetchSliderList();
-	}, [latitude, longitude]);
-
-	const fetchSliderList = () => {
-		// onLoaderFinished(30);
-		fetchDataFromApi(
-			`v5?lat=${latitude}&lng=${longitude}&page_type=DESKTOP_WEB_LISTING`,
-		).then((data) => {
-			console.log(data?.data?.cards[0]?.data?.data?.cards);
-			// onLoaderFinished(60);
-
-			setSliderResturantsList(data?.data?.cards[0]?.data?.data?.cards);
-			// onLoaderFinished(100);
-		});
-	};
+const Body = () => {
 	return (
-		<>
-			{sliderResturantsList?.length === 0 ? (
-				<ShimmerSlider />
-			) : (
-				<Slider {...settings} className="grid grid-cols-4 gap-14 justify-evenly">
-					{sliderResturantsList.map((item, index) => (
-						<div className="" key={index}>
-							<div key={item.data.bannerId} className="mr-[50px]">
-								<a className="group d-block text-center shadow-sm" href="/">
-									<img
-										width="260"
-										height="260"
-										alt="#"
-										src={IMG_SLIDER_URL + item.data.creativeId}
-										className="img-fluid transition duration-150 ease-[cubic-bezier(.215,.61,.355,1)] group-hover:scale-105"
-									/>
-								</a>
-							</div>
-						</div>
-					))}
-				</Slider>
-			)}
-		</>
+	<>
+			<div className=" bg-[#171a29] py-0 px-[20px]">
+				<div className=" max-w-[1200px] min-w-[1200px] my-0 mx-auto relative flex items-center h-[340px] block">
+					<div className="w-[100%]">
+						<ItemSlider />
+					</div>
+				</div>
+			</div>
+			<Outlet/>
+			</>
 	);
 };
 
-export default ItemSlider;
+export default Body;
