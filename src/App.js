1e47ec88eba23655552c94b0cdaae238d02193f0
Bharat bhushan <43534123+bharat6138@users.noreply.github.com>
2023-02-28 15:51:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,18 +8,33 @@ import Header from "./component/Header";
 import ItemSlider from "./component/ItemSlider";
 import LeftNav from "./component/LeftNav";
 import Body from "./component/Body";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Feed from "./component/Feed";
+import Filter from "./component/Filter";
 // import LoadingBar from "react-top-loading-bar";
 function App() {
-	// const [progress, setProgress] = useState(0);
-	return (
-		<Provider store={store}>
-			<div className="relative pt-[80px] min-h-[100%] flex flex-col min-w-[1240px]">
-				<Header />
-				<LeftNav />
-				<Body />
-			</div>
-		</Provider>
-	);
+  // const [progress, setProgress] = useState(0);
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="relative pt-[80px] min-h-[100%] flex flex-col min-w-[1240px]">
+          <Header />
+          <LeftNav />
+		  <div className=" flex-grow-[1] ">
+		 
+		  
+          <Routes>
+            <Route path="/" exact element={<Body />} >
+			<Route path="/" exact element={<Feed />} />
+			<Route path="/rating" exact element={<Feed />} />
+			<Route path="/delivery" exact element={<Feed />} />
+			</Route>
+          </Routes>
+        </div>
+		</div>
+      </BrowserRouter>
+    </Provider>
+  );
 }
 
 export default App;
