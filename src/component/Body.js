a3ce0b53294e44d8ca0578110d58be9a4e30e4fa
Bharat bhushan <43534123+bharat6138@users.noreply.github.com>
2023-03-03 11:22:45 +0530

    Update Body.js

--- src/component/Body.js
+++ src/component/Body.js
@@ -4,18 +4,18 @@ import Feed from "./Feed";
 import ItemSlider from "./ItemSlider";
 
 const Body = () => {
-	return (
-	<>
-			<div className=" bg-[#171a29] py-0 px-[20px]">
-				<div className=" max-w-[1200px] min-w-[1200px] my-0 mx-auto relative flex items-center h-[340px] block">
-					<div className="w-[100%]">
-						<ItemSlider />
-					</div>
-				</div>
-			</div>
-			<Outlet/>
-			</>
-	);
+  return (
+    <>
+      <div className=" bg-[#171a29] py-0 px-[20px]">
+        <div className=" max-w-[1200px] min-w-[1200px] my-0 mx-auto relative flex items-center h-[340px] block">
+          <div className="w-[100%]">
+            <ItemSlider />
+          </div>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
 };
 
 export default Body;
